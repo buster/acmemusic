89f36a06801b24889468581833a63abbedbcbf48
buster <me@bstr.eu>
2024-10-06 16:38:45 +0200

    16

--- src/main/java/de/acme/musicplayer/application/domain/model/Benutzer.java
+++ src/main/java/de/acme/musicplayer/application/domain/model/Benutzer.java
@@ -1,14 +1,43 @@
 package de.acme.musicplayer.application.domain.model;
 
-public class Benutzer {
-    private final String benutzername;
-    private final String passwort;
-    private final String email;
 
-    public Benutzer(String benutzername, String passwort, String email) {
-        this.benutzername = benutzername;
+import static com.google.common.base.Preconditions.checkArgument;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+public class Benutzer {
+    private final Name name;
+    private final Email email;
+    private Passwort passwort;
+
+    public Benutzer(Name name, Passwort passwort, Email email) {
+        this.name = name;
         this.passwort = passwort;
         this.email = email;
     }
 
+    public static class Name {
+        public final String benutzername;
+
+        public Name(String benutzername) {
+            checkArgument(isNotBlank(benutzername), "Benutzername darf nicht leer sein");
+            this.benutzername = benutzername;
+        }
+    }
+
+    public static class Passwort {
+        private final String passwort;
+
+        public Passwort(String passwort) {
+            checkArgument(isNotBlank(passwort), "Passwort darf nicht leer sein");
+            this.passwort = passwort;
+        }
+    }
+
+    public static class Email {
+        private final String email;
+
+        public Email(String email) {
+            this.email = email;
+        }
+    }
 }
