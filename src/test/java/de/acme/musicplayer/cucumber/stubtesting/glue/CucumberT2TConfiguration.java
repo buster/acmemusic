30a8328435c567b9a312e19a561ce31574e5e1cf
buster <me@bstr.eu>
2024-10-06 16:38:45 +0200

    8

--- src/test/java/de/acme/musicplayer/cucumber/stubtesting/glue/CucumberT2TConfiguration.java
+++ src/test/java/de/acme/musicplayer/cucumber/stubtesting/glue/CucumberT2TConfiguration.java
@@ -1,10 +1,10 @@
-package de.acme.musicplayer.cucumber.stubtesting;
+package de.acme.musicplayer.cucumber.stubtesting.glue;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @CucumberContextConfiguration
-@SpringBootTest(classes = StubConfiguration.class)
-public class CucumberConfiguration {
+@SpringBootTest(classes = T2TConfiguration.class)
+public class CucumberT2TConfiguration {
 
 }
