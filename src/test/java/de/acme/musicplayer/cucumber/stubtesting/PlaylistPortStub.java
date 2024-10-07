ecfa19e54ffed44138689b58496b5503a5fdac50
buster <me@bstr.eu>
2024-10-07 20:24:59 +0200

    5

---
+++ src/test/java/de/acme/musicplayer/cucumber/stubtesting/PlaylistPortStub.java
@@ -0,0 +1,27 @@
+package de.acme.musicplayer.cucumber.stubtesting;
+
+import de.acme.musicplayer.application.domain.model.Playlist;
+import de.acme.musicplayer.application.ports.PlaylistPort;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlaylistPortStub implements PlaylistPort {
+
+    private Map<String, Playlist> playlists = new HashMap<>();
+
+
+    @Override
+    public void addSongToPlaylist(String songId, String playlistId) {
+
+    }
+
+    @Override
+    public Playlist lade(String benutzername, String playlistName) {
+        Playlist playlist = playlists.get(playlistName);
+        if (playlist.getBesitzer().equals(benutzername)) {
+            return playlist;
+        }
+        return null;
+    }
+}
