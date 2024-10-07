63ce410e9c5fe376640b8dc165f052503f72e37c
buster <me@bstr.eu>
2024-10-07 21:51:55 +0200

    7

--- src/main/java/de/acme/musicplayer/application/ports/PlaylistPort.java
+++ src/main/java/de/acme/musicplayer/application/ports/PlaylistPort.java
@@ -1,10 +1,11 @@
 package de.acme.musicplayer.application.ports;
 
+import de.acme.musicplayer.application.domain.model.Lied;
 import de.acme.musicplayer.application.domain.model.Playlist;
 
 public interface PlaylistPort {
 
-    void addSongToPlaylist(String songId, String playlistId);
+    void addSongToPlaylist(Lied.LiedId liedId, Playlist.PlaylistId playlistId);
 
     Playlist lade(String benutzername, String playlistName);
 }
--- src/test/java/de/acme/musicplayer/cucumber/stubtesting/PlaylistPortStub.java
+++ src/test/java/de/acme/musicplayer/cucumber/stubtesting/PlaylistPortStub.java
@@ -1,30 +1,50 @@
 package de.acme.musicplayer.cucumber.stubtesting;
 
+import de.acme.musicplayer.application.domain.model.Lied;
 import de.acme.musicplayer.application.domain.model.Playlist;
 import de.acme.musicplayer.application.ports.PlaylistPort;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
+import static java.util.Arrays.asList;
+
 
 public class PlaylistPortStub implements PlaylistPort {
 
-    private Map<String, Playlist> playlists = new HashMap<>();
+    private Map<String, List<Playlist>> playlists = new HashMap<>();
 
 
     @Override
-    public void addSongToPlaylist(String songId, String playlistId) {
-
+    public void addSongToPlaylist(Lied.LiedId liedId, Playlist.PlaylistId playlistId) {
+        playlists.values().stream().flatMap(List::stream)
+                .filter(p -> p.getId().equals(playlistId))
+                .findFirst()
+                .ifPresent(p -> p.addLied(liedId));
     }
 
     @Override
     public Playlist lade(String benutzername, String playlistName) {
-        Playlist playlist = playlists.get(playlistName);
-        if (playlist == null) {
-            return null;
+        List<Playlist> userPlaylists = playlists.get(benutzername);
+        if (userPlaylists == null) {
+            return newPlaylist(benutzername, playlistName);
         }
-        if (playlist.getBesitzer().equals(benutzername)) {
+        return userPlaylists.stream()
+                .filter(p ->
+                        p.getId().equals(new Playlist.PlaylistId(benutzername, playlistName)))
+                .findFirst()
+                .orElse(newPlaylist(benutzername, playlistName));
+    }
+
+    private Playlist newPlaylist(String benutzername, String playlistName) {
+        if (playlists.containsKey(benutzername)) {
+            List<Playlist> userPlaylists = playlists.get(benutzername);
+            Playlist playlist = new Playlist(benutzername, playlistName);
+            userPlaylists.add(playlist);
+            playlists.put(benutzername, userPlaylists);
             return playlist;
         }
-        return null;
+        Playlist playlist = new Playlist(benutzername, playlistName);
+        playlists.put(benutzername, new ArrayList<>(Arrays.asList(playlist)));
+        return playlist;
     }
 }
