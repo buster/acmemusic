08078a8d20b4ad547ac03d40a013dfaba038a5d5
buster <me@bstr.eu>
2024-10-07 19:41:44 +0200

    4

---
+++ src/main/java/de/acme/musicplayer/application/domain/LiedAdministrationService.java
@@ -0,0 +1,18 @@
+package de.acme.musicplayer.application.domain;
+
+import de.acme.musicplayer.application.ports.LiedPort;
+import de.acme.musicplayer.application.usecases.LiedAdministrationUsecase;
+
+public class LiedAdministrationService implements LiedAdministrationUsecase {
+
+    private final LiedPort liedPort;
+
+    public LiedAdministrationService(LiedPort liedPort) {
+        this.liedPort = liedPort;
+    }
+
+    @Override
+    public long zähleLieder() {
+        return liedPort.zähleLieder();
+    }
+}
--- src/main/java/de/acme/musicplayer/application/domain/LiedHochladenService.java
+++ src/main/java/de/acme/musicplayer/application/domain/LiedHochladenService.java
@@ -1,21 +1,21 @@
 package de.acme.musicplayer.application.domain;
 
 import de.acme.musicplayer.application.domain.model.Lied;
-import de.acme.musicplayer.application.ports.LiedHinzufügenPort;
+import de.acme.musicplayer.application.ports.LiedPort;
 import de.acme.musicplayer.application.usecases.LiedHochladenUseCase;
 
 import java.net.URI;
 
 public class LiedHochladenService implements LiedHochladenUseCase {
 
-    private final LiedHinzufügenPort liedHinzufügenPort;
+    private final LiedPort liedPort;
 
-    public LiedHochladenService(LiedHinzufügenPort liedHinzufügenPort) {
-        this.liedHinzufügenPort = liedHinzufügenPort;
+    public LiedHochladenService(LiedPort liedPort) {
+        this.liedPort = liedPort;
     }
 
     @Override
     public void liedHochladen(String title, String artist, String album, String genre, String releaseYear, URI uri) {
-        liedHinzufügenPort.fügeLiedHinzu(new Lied(title, artist, album, genre, releaseYear, uri));
+        liedPort.fügeLiedHinzu(new Lied(title, artist, album, genre, releaseYear, uri));
     }
 }
