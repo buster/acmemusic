ca101bb4f6f563b88ed6f4c3c91402176ed59394
buster <me@bstr.eu>
2024-10-07 19:16:31 +0200

    1

--- src/main/java/de/acme/musicplayer/application/domain/MusicService.java
+++ src/main/java/de/acme/musicplayer/application/domain/MusicService.java
@@ -1,9 +1,10 @@
 package de.acme.musicplayer.application.domain;
 
 import de.acme.musicplayer.adapters.jpa.SongRepository;
+import de.acme.musicplayer.application.ports.LoadSongPort;
 
 public class MusicService {
 
-    private SongRepository songRepository;
+    private LoadSongPort loadSongPort;
 
 }
