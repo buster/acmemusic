e55a846e9974cafd2d458e96dc7ffcb174332acf
buster <me@bstr.eu>
2024-10-07 19:35:56 +0200

    3

---
+++ src/test/java/de/acme/musicplayer/cucumber/stubtesting/LiedPortStub.java
@@ -0,0 +1,47 @@
+package de.acme.musicplayer.cucumber.stubtesting;
+
+import de.acme.musicplayer.application.domain.model.Artist;
+import de.acme.musicplayer.application.domain.model.Lied;
+import de.acme.musicplayer.application.ports.LiedHinzufügenPort;
+import de.acme.musicplayer.application.ports.LiedLadenPort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LiedPortStub implements LiedLadenPort, LiedHinzufügenPort {
+
+    private final List<Lied> lieder = new ArrayList<Lied>();
+
+    @Override
+    public Lied ladeLied(String songId) {
+        System.out.println("Load Lied Port Stub!");
+        return lieder.get(Integer.parseInt(songId));
+    }
+
+    @Override
+    public void updateSong(Lied lied) {
+
+    }
+
+    @Override
+    public Lied findSongByArtist(Artist artist) {
+        return null;
+    }
+
+    @Override
+    public Long getPlayedSecondsByArtist(Artist artist) {
+        return 0L;
+    }
+
+    @Override
+    public Long zähleLieder() {
+        return Long.valueOf(lieder.size());
+    }
+
+    @Override
+    public void fügeLiedHinzu(Lied lied) {
+        lied.setId(String.valueOf(lieder.size()));
+        lieder.add(lied);
+        System.out.println("Add Lied Port Stub!");
+    }
+}
