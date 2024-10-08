ac88fb0bf642f6081d4c91b138d6554e28dd7030
buster <me@bstr.eu>
2024-10-08 22:06:33 +0200

    13

--- src/test/java/de/acme/musicplayer/cucumber/stubtesting/test2real/CucumberT2RConfiguration.java
+++ src/test/java/de/acme/musicplayer/cucumber/stubtesting/test2real/CucumberT2RConfiguration.java
@@ -8,27 +8,4 @@ import org.springframework.test.annotation.DirtiesContext;
 @SpringBootTest(classes = T2RConfiguration.class)
 @DirtiesContext
 public class CucumberT2RConfiguration {
-
-//    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
-//            "postgres:16-alpine"
-//    );
-//    @LocalServerPort
-//    private Integer port;
-
-//    @BeforeAll
-//    static void beforeAll() {
-//        postgres.start();
-//    }
-//
-//    @AfterAll
-//    static void afterAll() {
-//        postgres.stop();
-//    }
-//
-//    @DynamicPropertySource
-//    static void configureProperties(DynamicPropertyRegistry registry) {
-//        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-//        registry.add("spring.datasource.username", postgres::getUsername);
-//        registry.add("spring.datasource.password", postgres::getPassword);
-//    }
 }
