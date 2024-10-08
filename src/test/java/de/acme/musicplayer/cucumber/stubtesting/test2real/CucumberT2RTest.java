30291d796557f4948610a1957928c8a9c1ff9a1b
buster <me@bstr.eu>
2024-10-08 21:55:56 +0200

    12

--- src/test/java/de/acme/musicplayer/cucumber/stubtesting/test2real/CucumberT2RTest.java
+++ src/test/java/de/acme/musicplayer/cucumber/stubtesting/test2real/CucumberT2RTest.java
@@ -6,6 +6,7 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.core.options.Constants.FEATURES_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.FILTER_TAGS_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
@@ -13,5 +14,6 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @SelectPackages("de.acme")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "de.acme.musicplayer.cucumber.stubtesting.test2real")
 @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@T2R")
 public class CucumberT2RTest {
 }
