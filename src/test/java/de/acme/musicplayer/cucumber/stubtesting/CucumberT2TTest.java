f4478c2478e995189c5be9ab9e5280e1b4ed7fd8
buster <me@bstr.eu>
2024-10-08 18:51:59 +0200

    8

--- src/test/java/de/acme/musicplayer/cucumber/stubtesting/CucumberT2TTest.java
+++ src/test/java/de/acme/musicplayer/cucumber/stubtesting/CucumberT2TTest.java
@@ -5,12 +5,13 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.core.options.Constants.FEATURES_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectPackages("de.acme")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "de.acme.musicplayer.cucumber.stubtesting.glue")
-//@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features")
-public class CucumberStubTest {
+@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features")
+public class CucumberT2TTest {
 }
