80352a90147e794841c33fd9a2b50bf236a2aa8d
buster <me@bstr.eu>
2024-10-10 22:25:39 +0200

    26

--- src/generated/java/de/acme/jooq/Tables.java
+++ src/generated/java/de/acme/jooq/Tables.java
@@ -13,7 +13,7 @@ import de.acme.jooq.tables.PlaylistLied;
 /**
  * Convenience access to all tables in public.
  */
-@SuppressWarnings({"all", "unchecked", "rawtypes", "this-escape"})
+@SuppressWarnings({ "all", "unchecked", "rawtypes", "this-escape" })
 public class Tables {
 
     /**
