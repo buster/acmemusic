f97192b44c47925856feade1f0cf05e06fbf71dd
buster <me@bstr.eu>
2024-10-11 19:59:21 +0200

    13

--- src/main/java/de/acme/musicplayer/adapters/jpa/JpaEntityToSongMapper.java
+++
@@ -1,9 +0,0 @@
-package de.acme.musicplayer.adapters.jpa;
-
-import de.acme.musicplayer.application.domain.model.Lied;
-
-public class JpaEntityToSongMapper {
-    public Lied toDomain(SongJpaEntity byId) {
-        return null;
-    }
-}
