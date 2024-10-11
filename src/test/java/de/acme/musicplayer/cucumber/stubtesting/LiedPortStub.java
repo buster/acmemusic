c6217fc907757a6ee69c4a250014ccc8d66c4f70
buster <me@bstr.eu>
2024-10-11 22:41:53 +0200

    27

--- src/test/java/de/acme/musicplayer/cucumber/stubtesting/LiedPortStub.java
+++ src/test/java/de/acme/musicplayer/cucumber/stubtesting/LiedPortStub.java
@@ -3,12 +3,15 @@ package de.acme.musicplayer.cucumber.stubtesting;
 import de.acme.musicplayer.application.domain.model.Lied;
 import de.acme.musicplayer.application.ports.LiedPort;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LiedPortStub implements LiedPort {
 
     private final List<Lied> lieder = new ArrayList<>();
+    private final List<byte[]> bytestreams = new ArrayList<>();
 
     @Override
     public Lied ladeLied(Lied.Id songId) {
@@ -22,8 +25,9 @@ public class LiedPortStub implements LiedPort {
     }
 
     @Override
-    public Lied.Id fügeLiedHinzu(Lied lied) {
+    public Lied.Id fügeLiedHinzu(Lied lied, InputStream inputStream) throws IOException {
         lied.setId(new Lied.Id(String.valueOf(lieder.size())));
+        bytestreams.add(lieder.size(), inputStream.readAllBytes());
         lieder.add(lied);
         return lied.getId();
     }
