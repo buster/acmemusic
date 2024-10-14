c2e66ce2948b3567a740ac551e1c64c2f6ec3d1d
buster <me@bstr.eu>
2024-10-14 22:46:51 +0200

    44

--- src/main/java/de/acme/musicplayer/application/domain/BenutzerRegistrierenService.java
+++ src/main/java/de/acme/musicplayer/application/domain/BenutzerRegistrierenService.java
@@ -4,8 +4,10 @@ import de.acme.musicplayer.application.domain.model.Benutzer;
 import de.acme.musicplayer.application.domain.model.TenantId;
 import de.acme.musicplayer.application.ports.BenutzerPort;
 import de.acme.musicplayer.application.usecases.BenutzerRegistrierenUsecase;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 public class BenutzerRegistrierenService implements BenutzerRegistrierenUsecase {
 
     private final BenutzerPort benutzerPort;
@@ -18,6 +20,8 @@ public class BenutzerRegistrierenService implements BenutzerRegistrierenUsecase 
     @Transactional
     public Benutzer.Id registriereBenutzer(BenutzerRegistrierenCommand benutzerRegistrierenCommand) {
         Benutzer benutzer = new Benutzer(benutzerRegistrierenCommand.name(), benutzerRegistrierenCommand.passwort(), benutzerRegistrierenCommand.email());
-        return benutzerPort.benutzerHinzufügen(benutzer, benutzerRegistrierenCommand.tenant());
+        Benutzer.Id id = benutzerPort.benutzerHinzufügen(benutzer, benutzerRegistrierenCommand.tenant());
+        log.info("Benutzer {} registriert, ID: {}", benutzer.getName().benutzername, id.Id());
+        return id;
     }
 }
