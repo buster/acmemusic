9b184f9899cb023d7ff4d01f85b2a3809012220d
buster <me@bstr.eu>
2024-10-20 17:08:00 +0200

    86

--- src/main/java/de/acme/musicplayer/applications/users/domain/BenutzerIstTopScorerService.java
+++ src/main/java/de/acme/musicplayer/applications/users/domain/BenutzerIstTopScorerService.java
@@ -1,19 +1,36 @@
 package de.acme.musicplayer.applications.users.domain;
 
+import de.acme.musicplayer.applications.users.domain.model.Auszeichnung;
+import de.acme.musicplayer.applications.users.domain.model.Benutzer;
 import de.acme.musicplayer.applications.users.ports.BenutzerPort;
 import de.acme.musicplayer.applications.users.usecases.BenutzerIstTopScorerUsecase;
+import de.acme.musicplayer.applications.users.usecases.UserEventPublisher;
+import de.acme.musicplayer.events.BenutzerHatAuszeichnungVerlorenEvent;
+import de.acme.musicplayer.events.BenutzerHatNeueAuszeichnungEvent;
 import de.acme.musicplayer.events.NeuerTopScorerEvent;
+import org.springframework.transaction.annotation.Transactional;
 
 public class BenutzerIstTopScorerService implements BenutzerIstTopScorerUsecase {
 
     private final BenutzerPort benutzerPort;
+    private final UserEventPublisher userEventPublisher;
 
-    public BenutzerIstTopScorerService(BenutzerPort benutzerPort) {
+    public BenutzerIstTopScorerService(BenutzerPort benutzerPort, UserEventPublisher userEventPublisher) {
         this.benutzerPort = benutzerPort;
+        this.userEventPublisher = userEventPublisher;
     }
 
     @Override
+    @Transactional
     public void neuerTopScorerGefunden(NeuerTopScorerEvent event) {
+        Benutzer neuerTopScorer = benutzerPort.leseBenutzer(event.neuerTopScorer(), event.tenantId());
+        neuerTopScorer.getAuszeichnungen().add(Auszeichnung.MUSIC_LOVER_LOVER);
+        userEventPublisher.publishEvent(new BenutzerHatNeueAuszeichnungEvent(neuerTopScorer.getId(), Auszeichnung.MUSIC_LOVER_LOVER, event.tenantId()));
+        if (event.alterTopScorer() != null) {
+            Benutzer alterTopScorer = benutzerPort.leseBenutzer(event.alterTopScorer(), event.tenantId());
+            alterTopScorer.getAuszeichnungen().remove(Auszeichnung.MUSIC_LOVER_LOVER);
+            userEventPublisher.publishEvent(new BenutzerHatAuszeichnungVerlorenEvent(alterTopScorer.getId(), Auszeichnung.MUSIC_LOVER_LOVER, event.tenantId()));
 
+        }
     }
 }
