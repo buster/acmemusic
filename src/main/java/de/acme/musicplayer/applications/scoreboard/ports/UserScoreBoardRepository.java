781edac1acef87afb65bd2219abe4aaa8444b18e
buster <me@bstr.eu>
2024-10-22 18:56:40 +0200

    86

--- src/main/java/de/acme/musicplayer/applications/scoreboard/ports/UserScoreBoardRepository.java
+++ src/main/java/de/acme/musicplayer/applications/scoreboard/ports/UserScoreBoardRepository.java
@@ -1,9 +1,12 @@
 package de.acme.musicplayer.applications.scoreboard.ports;
 
+import de.acme.musicplayer.applications.musicplayer.domain.model.TenantId;
 import de.acme.musicplayer.applications.users.domain.model.Benutzer;
 
 public interface UserScoreBoardRepository {
-    void zähleNeuesLied(Benutzer.Id benutzerId);
+    void zähleNeuesLied(Benutzer.Id benutzerId, TenantId tenant);
 
     Benutzer.Id höchstePunktZahl();
+
+    void löscheDatenbank(TenantId tenantId);
 }
