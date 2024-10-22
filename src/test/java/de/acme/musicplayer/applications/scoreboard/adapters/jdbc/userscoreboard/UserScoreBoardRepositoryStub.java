47033819990635ad21900dbc9ff4a8deebe7cbce
buster <me@bstr.eu>
2024-10-22 18:23:41 +0200

    86

--- src/test/java/de/acme/musicplayer/applications/scoreboard/adapters/jdbc/userscoreboard/UserScoreBoardRepositoryStub.java
+++ src/test/java/de/acme/musicplayer/applications/scoreboard/adapters/jdbc/userscoreboard/UserScoreBoardRepositoryStub.java
@@ -1,9 +1,10 @@
 package de.acme.musicplayer.applications.scoreboard.adapters.jdbc.userscoreboard;
 
+import de.acme.musicplayer.applications.musicplayer.domain.model.TenantId;
 import de.acme.musicplayer.applications.scoreboard.ports.UserScoreBoardRepository;
 import de.acme.musicplayer.applications.users.domain.model.Benutzer;
+import org.apache.commons.lang3.tuple.MutablePair;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -11,18 +12,34 @@ import static java.util.Comparator.comparingInt;
 
 public class UserScoreBoardRepositoryStub implements UserScoreBoardRepository {
 
-    private ConcurrentHashMap<Benutzer.Id, Integer> scoreBoard = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<MutablePair<Benutzer.Id, TenantId>, Integer> scoreBoard = new ConcurrentHashMap<>();
 
     @Override
-    public void zähleNeuesLied(Benutzer.Id benutzerId) {
-        scoreBoard.put(benutzerId, scoreBoard.getOrDefault(benutzerId, 0) + 1);
+    public void zähleNeuesLied(Benutzer.Id benutzerId, TenantId tenant) {
+        scoreBoard.entrySet().stream()
+                .filter(entry -> entry.getKey().getLeft().equals(benutzerId))
+                .filter(entry -> entry.getKey().getRight().equals(tenant))
+                .findFirst()
+                .ifPresentOrElse(
+                        entry -> scoreBoard.put(entry.getKey(), entry.getValue() + 1),
+                        () -> scoreBoard.put(new MutablePair<>(benutzerId, tenant), 1)
+                );
     }
 
     @Override
     public Benutzer.Id höchstePunktZahl() {
         return scoreBoard.entrySet().stream()
                 .max(comparingInt(Map.Entry::getValue))
-                .map(HashMap.Entry::getKey)
+                .map(mutablePairIntegerEntry -> mutablePairIntegerEntry.getKey().getLeft())
                 .orElse(null);
+    }
+
+    @Override
+    public void löscheDatenbank(TenantId tenantId) {
+        for (Map.Entry<MutablePair<Benutzer.Id, TenantId>, Integer> mutablePairIntegerEntry : scoreBoard.entrySet()) {
+            if (mutablePairIntegerEntry.getKey().getRight().equals(tenantId)) {
+                scoreBoard.remove(mutablePairIntegerEntry.getKey());
+            }
+        }
     }
 }
