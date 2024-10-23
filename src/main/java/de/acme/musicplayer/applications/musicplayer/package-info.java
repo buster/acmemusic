b70daa7eb15a85f09b4b59773c1bb3fc9ce53557
buster <me@bstr.eu>
2024-10-23 16:33:55 +0200

    95

--- src/main/java/de/acme/musicplayer/applications/musicplayer/package-info.java
+++ src/main/java/de/acme/musicplayer/applications/musicplayer/package-info.java
@@ -2,7 +2,6 @@
         name = "Musicplayer",
         allowedDependencies = {"Users", "Scoreboard", "Events"},
         exposedPackages = {"de.acme.musicplayer.applications.musicplayer.usecases",
-                "de.acme.musicplayer.applications.musicplayer.domain.model",
                 "de.acme.musicplayer.applications.musicplayer.domain.events"}
 )
 package de.acme.musicplayer.applications.musicplayer;
