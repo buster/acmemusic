e5a619bdeda38779447fbd2acda9d05a2a52bd5d
buster <me@bstr.eu>
2024-10-24 21:50:10 +0200

    49 - first song played

--- src/main/java/de/acme/musicplayer/application/ports/LiedPort.java
+++ src/main/java/de/acme/musicplayer/application/ports/LiedPort.java
@@ -1,10 +1,12 @@
 package de.acme.musicplayer.application.ports;
 
+import de.acme.musicplayer.application.domain.model.Benutzer;
 import de.acme.musicplayer.application.domain.model.Lied;
 import de.acme.musicplayer.application.domain.model.TenantId;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collection;
 
 public interface LiedPort {
 
@@ -17,4 +19,6 @@ public interface LiedPort {
     void löscheDatenbank(TenantId tenantId);
 
     InputStream ladeLiedStream(Lied.Id liedId, TenantId tenantId);
+
+    Collection<Lied.Id> listeLiederAuf(Benutzer.Id benutzerId, TenantId tenantId);
 }
