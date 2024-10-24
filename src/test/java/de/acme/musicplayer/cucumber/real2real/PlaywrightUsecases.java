6ce27330aa2a93a6644dc5eb0a1db39edd98e3fb
buster <me@bstr.eu>
2024-10-24 19:11:10 +0200

    94

--- src/test/java/de/acme/musicplayer/cucumber/real2real/PlaywrightUsecases.java
+++ src/test/java/de/acme/musicplayer/cucumber/real2real/PlaywrightUsecases.java
@@ -26,7 +26,7 @@ public class PlaywrightUsecases implements BenutzerRegistrierenUsecase {
 
     @Override
     public BenutzerId registriereBenutzer(BenutzerRegistrierenCommand benutzerRegistrierenCommand) {
-        try (Page page = browserContextComponent.getBrowserContext().browser().newPage()) {
+        try (Page page = browserContextComponent.getBrowserContext().browser().newPage()94) {
             page.navigate(String.format("http://localhost:%s/?tenantId=%s", port, tenantId));
             assertThat(page).hasTitle("ACME Music Player");
             page.click("#open-register-modal-button");
