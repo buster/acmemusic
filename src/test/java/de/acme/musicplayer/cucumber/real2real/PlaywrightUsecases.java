2f272c5424729eac6af409585df9857e86e9e289
buster <me@bstr.eu>
2024-10-25 20:58:53 +0200

    101 - parallel deactivated

--- src/test/java/de/acme/musicplayer/cucumber/real2real/PlaywrightUsecases.java
+++ src/test/java/de/acme/musicplayer/cucumber/real2real/PlaywrightUsecases.java
@@ -1,17 +1,27 @@
 package de.acme.musicplayer.cucumber.real2real;
 
+import com.microsoft.playwright.ElementHandle;
+import com.microsoft.playwright.JSHandle;
 import com.microsoft.playwright.Page;
+import de.acme.musicplayer.applications.musicplayer.usecases.LiedAbspielenUsecase;
 import de.acme.musicplayer.applications.users.usecases.BenutzerRegistrierenUsecase;
 import de.acme.musicplayer.common.BenutzerId;
+import de.acme.musicplayer.common.LiedId;
+import de.acme.musicplayer.common.TenantId;
 import io.cucumber.spring.ScenarioScope;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 @Component
 @ScenarioScope
-public class PlaywrightUsecases implements BenutzerRegistrierenUsecase {
+@Slf4j
+public class PlaywrightUsecases implements BenutzerRegistrierenUsecase, LiedAbspielenUsecase {
 
     private final BrowserContextComponent browserContextComponent;
     @Setter
@@ -26,7 +36,7 @@ public class PlaywrightUsecases implements BenutzerRegistrierenUsecase {
 
     @Override
     public BenutzerId registriereBenutzer(BenutzerRegistrierenCommand benutzerRegistrierenCommand) {
-        try (Page page = browserContextComponent.getBrowserContext().browser().newPage()) {
+        try (Page page = browserContextComponent.getBrowserContext().newPage()) {
             page.navigate(String.format("http://localhost:%s/?tenantId=%s", port, tenantId));
             assertThat(page).hasTitle("ACME Music Player");
             page.click("#open-register-modal-button");
@@ -37,6 +47,19 @@ public class PlaywrightUsecases implements BenutzerRegistrierenUsecase {
             page.waitForSelector("[data-testid=\"registration-successful-toast\"]");
             String id = page.context().cookies().stream().filter(cookie -> cookie.name.equals("userId")).findFirst().orElseThrow().value;
             return new BenutzerId(id);
+        }
+    }
+
+    @Override
+    public InputStream liedStreamen(LiedId liedId, TenantId tenantId) {
+        try (Page page = browserContextComponent.getBrowserContext().newPage()) {
+            page.navigate(String.format("http://localhost:%s/?tenantId=%s", port, tenantId.value()));
+            page.click("#nav-link-songlist");
+            ElementHandle elementHandle = page.waitForSelector(String.format("[data-testid=\"songlist-%s\"]", liedId.id()));
+            JSHandle duration1 = elementHandle.getProperty("duration");
+            double duration = Double.parseDouble(duration1.toString());
+            log.debug("Returning fake Inputstream for song with id {}", liedId.id());
+            return new ByteArrayInputStream(new byte[(int) duration]);
         }
     }
 }
