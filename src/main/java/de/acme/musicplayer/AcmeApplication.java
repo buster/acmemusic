904073a62c938a7c81e28c9fabaa48ad6253586d
buster <me@bstr.eu>
2024-10-28 21:21:13 +0100

    113

--- src/main/java/de/acme/musicplayer/AcmeApplication.java
+++ src/main/java/de/acme/musicplayer/AcmeApplication.java
@@ -3,9 +3,11 @@ package de.acme.musicplayer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@ExcludeFromJacocoGenerated
 @SpringBootApplication
 public class AcmeApplication {
 
+    @ExcludeFromJacocoGenerated
     public static void main(String[] args) {
         SpringApplication.run(AcmeApplication.class, args);
     }
