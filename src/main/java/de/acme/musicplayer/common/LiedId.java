7b05e781d8b863e4b119ec6d4bc63db17d7d2ac7
buster <me@bstr.eu>
2024-11-01 21:36:22 +0100

    113

--- src/main/java/de/acme/musicplayer/common/LiedId.java
+++ src/main/java/de/acme/musicplayer/common/LiedId.java
@@ -2,26 +2,11 @@ package de.acme.musicplayer.common;
 
 import de.acme.musicplayer.ModuleApi;
 
-import java.util.Objects;
-
 import static com.google.common.base.Preconditions.checkNotNull;
 
 @ModuleApi
 public record LiedId(String id) {
     public LiedId {
         checkNotNull(id);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        LiedId liedId = (LiedId) o;
-        return Objects.equals(id, liedId.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
     }
 }
