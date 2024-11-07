3005403f2768fc1c5d05be770957ca3d4655fdbe
buster <me@bstr.eu>
2024-11-07 20:54:39 +0100

    134

--- run_service.sh
+++ run_service.sh
@@ -6,4 +6,5 @@ cd $HOME/projects/acme || exit
 export SERVER_PORT=8081
 export SERVER_HOST=127.0.0.1
 
-./mvnw liquibase:dropAll liquibase:update spring-boot:run -DskipDocker -Dspring-boot.run.jvmArguments='-Dserver.address=127.0.0.1'
+./mvnw liquibase:dropAll liquibase:update spring-boot:repackage -DskipTests -DskipDocker -Dspring-boot.run.jvmArguments='-Dserver.address=127.0.0.1'
+java -jar target/acme-0.0.1-SNAPSHOT.jar -Dserver.address=127.0.0.1
