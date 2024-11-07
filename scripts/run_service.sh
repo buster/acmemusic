3ab8d4b285b2572ce5612d1bf240d3faae3676eb
buster <me@bstr.eu>
2024-11-07 22:37:50 +0100

    139

--- scripts/run_service.sh
+++ scripts/run_service.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH/.. || exit
+
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk use java 21.0.2-tem
-cd $HOME/projects/acme || exit
 export SERVER_PORT=8081
 export SERVER_HOST=127.0.0.1
 
