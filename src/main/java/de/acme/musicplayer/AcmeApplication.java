798c9472fd177743e239fdcdabfbc939e0359aa2
buster <me@bstr.eu>
2024-11-07 18:52:36 +0100

    Revert "136"
    This reverts commit f538a6306d34a226afb6ddde5237da57994ade26.

--- src/main/java/de/acme/musicplayer/AcmeApplication.java
+++ src/main/java/de/acme/musicplayer/AcmeApplication.java
@@ -2,9 +2,11 @@ package de.acme.musicplayer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @ExcludeFromJacocoGenerated
 @SpringBootApplication
+@ImportRuntimeHints(JooqRuntimeHintsRegistrar.class)
 public class AcmeApplication {
 
     @ExcludeFromJacocoGenerated
