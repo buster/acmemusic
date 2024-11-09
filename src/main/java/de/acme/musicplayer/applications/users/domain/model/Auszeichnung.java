64d0ef407c1dd17db9345c22f5406dbd495b6ac0
buster <me@bstr.eu>
2024-11-09 20:12:53 +0100

    86 - parallel testing off -> bug

--- src/main/java/de/acme/musicplayer/applications/users/domain/model/Auszeichnung.java
+++ src/main/java/de/acme/musicplayer/applications/users/domain/model/Auszeichnung.java
@@ -1,5 +1,8 @@
 package de.acme.musicplayer.applications.users.domain.model;
 
+import de.acme.musicplayer.ModuleApi;
+
+@ModuleApi
 public enum Auszeichnung {
     LAME_DUCK,
     MUSIC_LOVER_LOVER,
--- src/main/java/de/acme/musicplayer/events/BenutzerHatNeueAuszeichnungEvent.java
+++ src/main/java/de/acme/musicplayer/events/BenutzerHatNeueAuszeichnungEvent.java
@@ -1,9 +1,11 @@
 package de.acme.musicplayer.events;
 
+import de.acme.musicplayer.ModuleApi;
 import de.acme.musicplayer.applications.musicplayer.domain.model.TenantId;
 import de.acme.musicplayer.applications.users.domain.model.Auszeichnung;
 import de.acme.musicplayer.applications.users.domain.model.Benutzer;
 
+@ModuleApi
 public record BenutzerHatNeueAuszeichnungEvent(Benutzer.Id id, Auszeichnung auszeichnung,
                                                TenantId tenantId) implements Event {
     @Override
