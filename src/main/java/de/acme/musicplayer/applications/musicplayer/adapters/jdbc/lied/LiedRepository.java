743390f98000b059d9cbec824358f189bfc1e4af
buster <me@bstr.eu>
2024-11-11 20:27:53 +0100

    91

--- src/main/java/de/acme/musicplayer/applications/musicplayer/adapters/jdbc/lied/LiedRepository.java
+++ src/main/java/de/acme/musicplayer/applications/musicplayer/adapters/jdbc/lied/LiedRepository.java
@@ -1,5 +1,7 @@
 package de.acme.musicplayer.applications.musicplayer.adapters.jdbc.lied;
 
+import de.acme.jooq.tables.records.LiedAuszeichnungenRecord;
+import de.acme.jooq.tables.records.LiedRecord;
 import de.acme.musicplayer.applications.musicplayer.domain.model.Lied;
 import de.acme.musicplayer.applications.musicplayer.domain.model.LiedAuszeichnung;
 import de.acme.musicplayer.applications.musicplayer.domain.model.TenantId;
@@ -15,8 +17,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
 
+import static com.google.common.base.Preconditions.checkState;
 import static de.acme.jooq.Tables.LIED;
 import static de.acme.jooq.Tables.LIED_AUSZEICHNUNGEN;
+import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 
 @Component
 public class LiedRepository implements LiedPort {
@@ -66,14 +70,30 @@ public class LiedRepository implements LiedPort {
 
     @Override
     public Lied leseLied(Lied.Id id, TenantId tenantId) {
-        Result<Record> records = dslContext.select().from(LIED.leftOuterJoin(LIED_AUSZEICHNUNGEN).on(LIED_AUSZEICHNUNGEN.TENANT.eq(tenantId.value()).and(LIED_AUSZEICHNUNGEN.LIEDID.eq(id.id()))))
+        LiedRecord liedRecord = dslContext.selectFrom(LIED)
+                .where(LIED.ID.eq(id.id()).and(LIED.TENANT.eq(tenantId.value())))
+                .fetchOne();
+        checkState(liedRecord != null, "Lied nicht gefunden");
+
+        Lied lied = new Lied(
+                new Lied.Id(liedRecord.getId()),
+                new Lied.Titel(liedRecord.getTitel()),
+                new Benutzer.Id(liedRecord.getBesitzerId()),
+                new TenantId(liedRecord.getTenant()));
+
+
+        Result<LiedAuszeichnungenRecord> liedAuszeichnungen = dslContext.selectFrom(LIED_AUSZEICHNUNGEN
+                        .where(LIED_AUSZEICHNUNGEN.LIEDID.eq(id.id())
+                                .and(LIED_AUSZEICHNUNGEN.TENANT.eq(tenantId.value()))))
                 .fetch();
 
-        Lied lied = new Lied(new Lied.Id(records.getFirst().get(LIED.ID)), new Lied.Titel(records.getFirst().get(LIED.TITEL)), new Benutzer.Id(records.getFirst().get(LIED.BESITZER_ID)), new TenantId(records.getFirst().get(LIED.TENANT)));
-        lied.setAuszeichnungen(
-                records.stream()
-                        .filter(r -> r.get(LIED_AUSZEICHNUNGEN.AUSZEICHNUNG) != null)
-                        .map(r -> LiedAuszeichnung.valueOf(r.get(LIED_AUSZEICHNUNGEN.AUSZEICHNUNG))).toList());
+        if (isNotEmpty(liedAuszeichnungen)) {
+            lied.setAuszeichnungen(
+                    liedAuszeichnungen.stream()
+                            .map(r -> LiedAuszeichnung.valueOf(r.getAuszeichnung()))
+                            .toList());
+        }
+
         return lied;
     }
 }
