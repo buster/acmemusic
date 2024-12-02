41a047d9bd403d4158942eb07a49af5c1d260598
buster <me@bstr.eu>
2024-12-02 19:23:07 +0100

    129

---
+++ src/main/java/de/acme/musicplayer/applications/musicplayer/usecases/MusicplayerEventDispatcher.java
@@ -0,0 +1,6 @@
+package de.acme.musicplayer.applications.musicplayer.usecases;
+
+import de.acme.musicplayer.common.EventDispatcher;
+
+public interface MusicplayerEventDispatcher extends EventDispatcher {
+}
