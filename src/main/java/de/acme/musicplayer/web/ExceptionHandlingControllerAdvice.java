46210e5ffdd3b82c316d6ce024a948658af37d1d
buster <me@bstr.eu>
2024-12-12 21:06:23 +0100

    fixes pr

--- src/main/java/de/acme/musicplayer/web/ExceptionHandlingControllerAdvice.java
+++ src/main/java/de/acme/musicplayer/web/ExceptionHandlingControllerAdvice.java
@@ -1,7 +1,8 @@
 package de.acme.musicplayer.web;
 
-import io.github.wimdeblauwe.htmx.spring.boot.mvc.HtmxResponse;
-import io.github.wimdeblauwe.htmx.spring.boot.mvc.HtmxReswap;
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxReswap;
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRetarget;
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxSwapType;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,11 +13,9 @@ import java.util.Map;
 public class ExceptionHandlingControllerAdvice {
 
     @ExceptionHandler(Exception.class)
-    public HtmxResponse handleError(Exception ex) {
-        return HtmxResponse.builder()
-                .retarget("#toast-container")
-                .reswap(HtmxReswap.innerHtml())
-                .view(new ModelAndView("htmx-responses/error-toast.html", Map.of("message", ex.getMessage())))
-                .build();
+    @HxReswap(HxSwapType.INNER_HTML)
+    @HxRetarget("#toast-container")
+    public ModelAndView handleError(Exception ex) {
+        return new ModelAndView("htmx-responses/error-toast.html", Map.of("message", ex.getMessage()));
     }
 }
